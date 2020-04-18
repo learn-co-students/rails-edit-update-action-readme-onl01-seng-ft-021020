3550c41f75aee2079612fd71374d822e7822fa64
leighad <cdversiontwo@gmail.com>
2020-04-17 18:17:24 -0700

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    @article = Article.find_by_id(params[:id])
   end
 
   def new
@@ -12,12 +12,28 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new
-    @article.title = params[:title]
-    @article.description = params[:description]
-    @article.save
-    redirect_to article_path(@article)
+    # @article = Article.new
+    # @article.title = params[:title]
+    # @article.description = params[:description]
+    # @article.save
+    # redirect_to article_path(@article)
+
+    @article = Article.create(title: params[:article][:title], description: params[:article][:description])
+    if @article.save
+        redirect_to article_path(@article)
+    else
+        render :new 
+    end
   end
 
   # add edit and update methods here
+  def edit
+    @article = Article.find_by_id(params[:id])
+  end
+
+  def update
+    @article = Article.find_by_id(params[:id])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    redirect_to article_path(@article)
+  end
 end
